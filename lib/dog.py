13ba0eb212c0767d8cab207c509a53c08c03fd73
JOY254 <nyorgijoy@gmail.com>
2023-11-29 00:46:50 -0600

    working through

--- lib/dog.py
+++ lib/dog.py
@@ -13,3 +13,32 @@ APPROVED_BREEDS = [
 
 class Dog:
     pass
+    approved_breeds = ["Mastiff","Chihuahua","Corgi","Shar Pei","Beagle", "French Bulldog","Pug","Pointer"]
+
+
+    def __init__(self, name="", breed=""):
+        self._name = name
+        self._breed = breed
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if not isinstance(value, str) or not (1 <= len(value) <= 25):
+            print(f"Name must be a string between 1 and 25 characters.")
+        else:
+            self._name = value
+
+    @property
+    def breed(self):
+        return self._breed
+
+    @breed.setter
+    def breed(self, value):
+        if value not in self.approved_breeds:
+            print("Breed must be in the list of approved breeds.")
+        else:
+            self._breed = value
+
--- lib/person.py
+++ lib/person.py
@@ -15,5 +15,33 @@ APPROVED_JOBS = [
     "Purchasing"
 ]
 
+
 class Person:
     pass
+    approved_jobs = ["Admin","Customer Service","Human Resources","ITC","Production","Legal", "Finance","Sales","General Management", "Research & Development","Marketing", "Purchasing"]
+
+    def __init__(self, name="", job=""):
+        self._name = name
+        self._job = job
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, value):
+        if not isinstance(value, str) or len(value) > 25:
+            print(f"Name must be a string under 25 characters.")
+        else:
+            self._name = value.title()
+
+    @property
+    def job(self):
+        return self._job
+
+    @job.setter
+    def job(self, value):
+        if value not in self.approved_jobs:
+            print("Job must be in the list of approved jobs.")
+        else:
+            self._job = value
